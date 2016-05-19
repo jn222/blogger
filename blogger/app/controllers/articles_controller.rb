f56f89fcce830bae779020d352caee1bf02dd4a1
Jonathan Ng <jonathanng222@gmail.com>
2016-05-19 15:28:51 -0400

    I1

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,6 +1,38 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    flash.notice = "Article '#{@article.title}' Edited!"
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to articles_path
   end
 
   def show
